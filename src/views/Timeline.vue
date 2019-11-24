cd9492938cb8dd9988bd3a2fcb8c23e309f3d461
Danny Tan <tandanielm@gmail.com>
2019-11-23 16:24:21 -1000

    Adding apex chart package

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,11 +1,50 @@
 <template>
-  <div>
-    This is the timeline.
+  <div id="chart">
+    <apexchart height=350 :options="chartOptions" :series="series" />
   </div>
 </template>
 
 <script>
+import VueApexCharts from 'vue-apexcharts';
+
 export default {
   name: 'Timeline',
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      series: [{
+        name: 'Desktops',
+        data: [1, 5, 3, 2, 8, 4, 9, 7, 5],
+      }],
+      chartOptions: {
+        chart: {
+          zoom: {
+            enabled: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: 'straight',
+        },
+        title: {
+          text: 'My Timeline',
+          align: 'center',
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5,
+          },
+        },
+        xaxis: {
+          categories: ['1995', '2001', '2003', '2007', '2011', '2014', '2017', '2018', '2019'],
+        },
+      },
+    };
+  },
 };
 </script>

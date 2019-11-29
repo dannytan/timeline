0efabfb2bd1a3a05154736a89ccfba1f9c640028
Danny Tan <tandanielm@gmail.com>
2019-11-28 23:36:18 -1000

    Created the Add Event modal dialog

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,13 +1,81 @@
 <template>
   <div id="chart">
+    <el-row type="flex" align="middle">
+      <el-col :span="12">
+        <div class="chart-title">Spiritual Timeline</div>
+      </el-col>
+      <el-col :span="12">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          class="add-event-btn"
+          size="medium"
+          @click="openAddEventDialog">Add Event</el-button>
+      </el-col>
+    </el-row>
     <apexchart height=350 :options="chartOptions" :series="series" />
+    <el-dialog
+      class="add-event-dialog"
+      title="Add Event To Timeline"
+      :visible.sync="showAddEventDialog"
+      width="40%"
+      center>
+      <el-row class="dialog-row">
+        <el-col :span="24">
+          <div class="input-label">Title</div>
+          <el-input
+            v-model="newEvent.title"
+            clearable>
+          </el-input>
+        </el-col>
+      </el-row>
+      <el-row class="dialog-row">
+        <el-col :span="12" class="column-left">
+          <div class="input-label">Date</div>
+          <el-date-picker
+            class="full-width"
+            v-model="newEvent.date"
+            type="date"
+            placeholder="MM/DD/YYY"
+            format="MM/dd/yyyy"
+            value-format="MM-dd-yyyy">
+          </el-date-picker>
+        </el-col>
+        <el-col :span="12" class="column-right">
+          <div class="input-label">Rating (1-10)</div>
+          <el-input-number
+            class="full-width"
+            v-model="newEvent.rating"
+            controls-position="right"
+            :min="1"
+            :max="10">
+          </el-input-number>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <div class="input-label">Description (Optional)</div>
+          <el-input
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 4}"
+            v-model="newEvent.description">
+          </el-input>
+        </el-col>
+      </el-row>
+      <span slot="footer">
+    <el-button @click="closeAddEventDialog">Cancel</el-button>
+    <el-button
+      type="primary"
+      icon="el-icon-plus"
+      @click="addEvent">Add</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import VueApexCharts from 'vue-apexcharts';
 
-// TODO - Change to show data points at all times, not just on hover
 export default {
   name: 'Timeline',
   components: {
@@ -37,18 +105,23 @@ export default {
           zoom: {
             enabled: false,
           },
+          toolbar: {
+            show: false,
+          },
         },
         dataLabels: {
           enabled: false,
+        },
+        markers: {
+          size: 5,
+          hover: {
+            size: 7,
+          },
         },
         stroke: {
           curve: 'straight',
         },
         colors: ['#ba334f'],
-        title: {
-          text: 'My Timeline',
-          align: 'center',
-        },
         grid: {
           row: {
             colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
@@ -68,18 +141,83 @@ export default {
   },
   data() {
     return {
+      showAddEventDialog: false,
+      newEvent: {
+        title: '',
+        date: '',
+        rating: null,
+        description: '',
+      },
       events: [{
         title: 'Test Event',
-        date: '11-20-2019',
+        date: '11-20-2017',
         rating: 9,
         description: 'This is a test description.',
       }, {
         title: 'Test Event 2',
-        date: '11-28-2019',
+        date: '11-28-2018',
         rating: 3,
+        description: 'This is another test description.',
+      }, {
+        title: 'Test Event 2',
+        date: '11-30-2019',
+        rating: 6,
         description: 'This is another test description.',
       }],
     };
   },
+  methods: {
+    openAddEventDialog() {
+      this.showAddEventDialog = true;
+    },
+    closeAddEventDialog() {
+      this.showAddEventDialog = false;
+      this.clearNewEvent();
+    },
+    addEvent() {
+      this.events.push(this.newEvent);
+      this.closeAddEventDialog();
+    },
+    clearNewEvent() {
+      this.newEvent = {
+        title: '',
+        date: '',
+        rating: null,
+        description: '',
+      };
+    },
+  },
 };
 </script>
+
+<style scoped lang="scss">
+  .chart-title {
+    color: #873031;
+    font-size: 26px;
+    font-weight: bold;
+    padding: 5px 25px;
+  }
+
+  .add-event-btn {
+    float: right;
+  }
+
+  .add-event-dialog {
+    .input-label {
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+    .dialog-row {
+      margin-bottom: 20px;
+      .column-left {
+        padding-right: 10px;
+      }
+      .column-right {
+        padding-left: 10px;
+      }
+      .full-width {
+        width: 100%;
+      }
+    }
+  }
+</style>
